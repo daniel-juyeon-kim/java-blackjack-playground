3824872ab3c6eb77af0b3a1013b154c1dc3d1e16
Daniel-k-dev <juyeon10120@naver.com>
2024-05-09 20:36:10 +0900

    feat(blackjack): BlackJackMain 클래스 추가

---
+++ src/main/java/blackjack/BlackJackMain.java
@@ -0,0 +1,10 @@
+package blackjack;
+
+import blackjack.controller.BlackJackController;
+
+public class BlackJackMain {
+
+    public static void main(String[] args) {
+        BlackJackController.run();
+    }
+}
