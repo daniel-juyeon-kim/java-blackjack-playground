7e522a21fa02592db89907b4d5c775fa3c434a76
Daniel-k-dev <juyeon10120@naver.com>
2024-06-23 11:05:38 +0900

    feat(view): View 추상 클래스 추가

---
+++ src/main/java/blackjack/view/View.java
@@ -0,0 +1,11 @@
+package blackjack.view;
+
+public class View {
+    protected static void print(String message) {
+        System.out.println(message);
+    }
+
+    protected static void enterLine() {
+        System.out.println();
+    }
+}
